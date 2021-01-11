787cc8d759dab71e7919099311835c038d044123
Dhruv Prajapati <60540925+imdhruv99@users.noreply.github.com>
2021-01-11 21:32:56 +0530

    Added Chef And Ants with 30% Partial Solution

---
+++ January-Long-Challenge-2021/06_Chef_and_Ants.py
@@ -0,0 +1,32 @@
+## 30/100 Partical Solution
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    A = []
+    for _ in range(n):
+        A.append(list(map(int, input().split())))
+    
+    for a in A:
+        neg = 0
+        for i in range(1, len(a)):
+            if a[i] < 0:
+                neg+=1
+        #print(neg)
+        print(neg*(len(a)-neg -1))
+
+"""
+Sample Input
+
+1
+2
+2 -2 1
+1 2
+"""
+
+"""
+Sample Output
+
+2
+"""
